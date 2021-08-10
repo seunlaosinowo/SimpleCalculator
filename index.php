f12fe5c0fdbb578ec2aa01695cc2e87b9f6d1e12
Seunla Osinowo <seunlaosinowo@gmail.com>
2021-08-10 07:31:30 +0100

    no message

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+<body>
+	<h1>Basic PHP Calaculator</h1>
+		<form action="calc.php" method="POST">
+			<input type="text" name="num1" /> <BR>
+
+			<input type="text" name="num2" /> <BR>
+
+
+			
+				<select name="cal">
+					<option value="add"> ADD </option>
+					<option value="sub"> SUB</option>
+					<option value="mul"> MUL</option>
+				</select>
+
+				<br>
+				<button type="submit"> Calaculate</button>
+				<br>
+
+			<input type="submit" name="submit" value="Convert Currency"/>
+		</form>
+</body>
+
+</html>
